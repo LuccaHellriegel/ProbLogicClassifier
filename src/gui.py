8f95436d9891d119f63ec7ef5e23ea9f87c9f377
luccr1 <lucca.hellriegel@gmx.net>
2019-07-31 23:07:46 +0200

    todos for gui

--- src/gui.py
+++ src/gui.py
@@ -9,7 +9,7 @@ layout = QVBoxLayout()
 startButton = QPushButton('Start')
 
 def start():
-    accuracy, choices = model.search_rule(5000)
+    accuracy, choices = model.test_rule(model.search_rule(5000))
     accuracyLabel = QLabel("Accuracy: "+str(accuracy))
     choicesLabel = QLabel("Choices: "+str(choices))
     layout.addWidget(accuracyLabel)
@@ -21,3 +21,12 @@ layout.addWidget(startButton)
 window.setLayout(layout)
 window.show()
 app.exec_()
+
+# TODO
+# Show were the data is
+# Start = Search rule (split rule und testing)
+# Test = Accuracy + choices (better formatted -> maybe print sentences + evaluation) -> ask for feedback, make choice undoable
+## better make it not a list but something like with a slider -> next, prev
+# Ask = Ask a question from the found definitions in the test
+# Make option in the corner to just run everything
+# Extra option to test single sentence
